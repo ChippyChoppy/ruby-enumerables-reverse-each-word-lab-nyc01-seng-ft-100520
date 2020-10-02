faaffcd2488ed08ad3b6b07bba638bb289196eb8
Rebecca Robbins <rebeccamargaret13@gmail.com>
2020-10-02 02:28:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,9 @@
  def reverse_each_word(string)
-   new_array = string.split(" ")
-   reversed_array = []
-   new_array.each do |word|
-     reversed_array.push(word.reverse)
+   string_array = string.split(" ")
+   reverse_array = []
+   new_array.each { |word| reverse_array.push(word.reverse) }
    end
-   reversed_array.join(" ")
+   reverse_array.join(" ")
  end
 
 def reverse_each_word(string)
